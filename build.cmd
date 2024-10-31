ef5bf5a0d09ec8301cf6ec65408fd7fc71cd4b2d
markos <83114316+Splerger@users.noreply.github.com>
2024-10-31 19:40:41 +0000

    small update
    updated build.cmd
    slight change to readme.md

--- build.cmd
+++ build.cmd
@@ -2,4 +2,6 @@
 
 pyinstaller --onefile --icon=icon.ico --add-data="icon.ico;." .\calculator.pyw
 
+signtool sign /a /fd SHA256 /tr http://timestamp.digicert.com /td SHA256 dist/calculator.exe
+
 start dist/calculator.exe
